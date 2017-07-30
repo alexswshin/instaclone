6bed4e0ff0c78c3489a4cdf4c4d0d4e575683c77
ilovebeefsteak <alex.sw.shin@gmail.com>
2017-07-30 16:05:31 +0900

    json

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-alert("Hello World!!");
+// var weapons = ["AK47", "Magnum", "M16"];
+// var soldier = {
+//   name: "Sha Sha",
+//   age: 22,
+//   weapon: weapons[2],
+//   isKorean: true,
+//   isDead: false,
+//   sayHi: function(a){
+//     console.log("Hello "+this.name+" and I'm "+this.age + a);
+//   }
+// };
+//
+// function sum(a, b){
+//   return a+b;
+// }
+//
+// function sub(a, b){
+//   return a-b;
+// }
+//
+// function div(a, b){
+//   return a/b;
+// }
+//
+// function exp(a, b){
+//   c=1;
+//   for (i=0; i<b; i++)
+//   {
+//     c=c*a;
+//   }
+//   return c;
+// }
