bdf1f443f48c443798c1be9e3a9be40107a3ef3f
ilovebeefsteak <alex.sw.shin@gmail.com>
2017-07-30 17:44:52 +0900

    add toggling overlay

--- js/app.js
+++ js/app.js
@@ -1,32 +1,21 @@
-// var weapons = ["AK47", "Magnum", "M16"];
-// var soldier = {
-//   name: "Sha Sha",
-//   age: 22,
-//   weapon: weapons[2],
-//   isKorean: true,
-//   isDead: false,
-//   sayHi: function(a){
-//     console.log("Hello "+this.name+" and I'm "+this.age + a);
-//   }
-// };
-//
-// function sum(a, b){
-//   return a+b;
-// }
-//
-// function sub(a, b){
-//   return a-b;
-// }
-//
-// function div(a, b){
-//   return a/b;
-// }
-//
-// function exp(a, b){
-//   c=1;
-//   for (i=0; i<b; i++)
-//   {
-//     c=c*a;
-//   }
-//   return c;
-// }
+$(document).ready(function() {
+  var $heart = $(".heart");
+  $heart.click(function(){
+    $(this).toggleClass("heart-clicked fa-heart fa-heart-o");
+  });
+  var $profileOverlay = $(".profile__overlay");
+  var $closeBtn = $(".cancelLauncher");
+
+  $closeBtn.click(function(){
+    $profileOverlay.fadeOut();
+  });
+  var $openBtn = $(".fa-cog");
+  $openBtn.click(function(){
+    $profileOverlay.fadeIn();
+  })
+
+  var $logoutBtn = $(".logoutLauncher");
+  $logoutBtn.click(function(){
+    window.location = 'index.html'
+  })
+});
